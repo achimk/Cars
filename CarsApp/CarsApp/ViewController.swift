f6ff96caf5f58cea107167d54172135e79165d2e
Joachim Kret <joachim.kret@performgroup.com>
2017-07-29 16:02:14 +0200

    Added carthage dependencies.

---
+++ CarsApp/CarsApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CarsApp
+//
+//  Created by Joachim Kret on 29/07/2017.
+//  Copyright © 2017 Joachim Kret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let bag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Carthage/Checkouts/Reusable/Example/Pods/Target Support Files/Pods-ReusableDemo/Pods-ReusableDemo-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_ReusableDemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ReusableDemoVersionString[];
+
