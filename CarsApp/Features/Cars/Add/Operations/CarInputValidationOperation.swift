6e9f7eb5dd65e2ec88d40a15f84298a3773c2168
Joachim Kret <joachim.kret@performgroup.com>
2017-08-01 09:38:33 +0200

    Cleanups for view model.

---
+++ CarsApp/Features/Cars/Add/Operations/CarInputValidationOperation.swift
@@ -0,0 +1,9 @@
+//
+//  CarInputValidationOperation.swift
+//  CarsApp
+//
+//  Created by Joachim Kret on 01/08/2017.
+//  Copyright © 2017 Joachim Kret. All rights reserved.
+//
+
+import Foundation
